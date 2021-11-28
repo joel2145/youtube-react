e63fc340f997db9f4bd9f74922379ed6e1aefb39
KikuchiYuta <kikkun0513@gmail.com>
2021-11-28 19:44:27 +0900

    ヘッダー作成中

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
+
+import Style from "./Header.module.scss"
 
 const Header = () => {
   return (
-    <div>
-      Header
+
+    <div className={Style.header}>
+      <div>
+        <Link to="/">VideoTube</Link>
+      </div>
+      <div>
+        <form>
+          <input type="text" placeholder="検索"></input>
+          <button type="submit"><FontAwesomeIcon icon={faSearch} /> </button>
+        </form>
+      </div>
     </div>
+
   )
 }
 
